9349deb966c0f04c0adbfb41ea102b04521864b7
Dane <golightning225@gmail.com>
2024-05-08 12:55:49 +0800

    partial styling on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useContext } from 'react';
-import axios from 'axios';
 import { AuthContext } from '../middleware/AuthContext';
+import axios from 'axios';
+import styles from './Profile.module.css';
+import Button from '../components/Button';
+import FormControl from '../components/FormControl';
 
 const Profile = () => {
   const { user } = useContext(AuthContext);
@@ -24,6 +27,10 @@ const Profile = () => {
 
   const handleInputChange = (e) => {
     setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const handleCancel = () => {
+    setEditMode(false);
   };
 
   const handleUpdate = () => {
@@ -69,70 +76,83 @@ const Profile = () => {
   };
 
   return (
-    <div>
-      <h1>Profile</h1>
-      <div>
-        <label>Image:</label>
-        {editMode ? (
-          <input
-            type='text'
-            name='image'
-            value={userData.image}
-            onChange={handleInputChange}
-          />
-        ) : (
-          <img src={`${userData.image}`} alt='Profile' />
-        )}
+    <>
+      <h1>Portfolio</h1>
+      <div className={styles.container}>
+        <div className={styles.Profile}>
+          <h3>Hello {user.name}</h3>
+          <div>
+            <img src={userData.image} alt={userData.name} />
+          </div>
+          <div>
+            <h3>Name:</h3>
+            {editMode ? (
+              <FormControl
+                type={'text'}
+                name={'name'}
+                id={'name'}
+                value={userData.name}
+                change={handleInputChange}
+              />
+            ) : (
+              <p>{userData.name}</p>
+            )}
+          </div>
+          <div>
+            <h3>Student Number:</h3>
+            {editMode ? (
+              <FormControl
+                type={'text'}
+                name={'studentNumber'}
+                id={'studentNumber'}
+                value={userData.studentNumber}
+                change={handleInputChange}
+              />
+            ) : (
+              <p>{userData.studentNumber}</p>
+            )}
+          </div>
+          <div>
+            <h3>Email:</h3>
+            {editMode ? (
+              <FormControl
+                type={'email'}
+                name={'email'}
+                id={'email'}
+                value={userData.email}
+                change={handleInputChange}
+              />
+            ) : (
+              <p>{userData.email}</p>
+            )}
+          </div>
+          {editMode ? (
+            <>
+              <Button
+                click={handleUpdate}
+                text={'Save'}
+                color={'btn-dark'}
+                size={'btn-block'}
+              />
+              <Button click={handleCancel} text={'Cancel'} size={'btn-block'} />
+            </>
+          ) : (
+            <Button
+              click={handleEdit}
+              text={'Edit'}
+              color={'btn-primary'}
+              size={'btn-block'}
+            />
+          )}
+        </div>
+        <div className={styles.Widgets}>
+          <h3>Stats</h3>
+        </div>
+        <div className={styles.Photos}>
+          <h3>Photos</h3>
+        </div>
       </div>
-      <div>
-        <label>Name:</label>
-        {editMode ? (
-          <input
-            type='text'
-            name='name'
-            value={userData.name}
-            onChange={handleInputChange}
-          />
-        ) : (
-          <span>{userData.name}</span>
-        )}
-      </div>
-      <div>
-        <label>Student Number:</label>
-        {editMode ? (
-          <input
-            type='text'
-            name='studentNumber'
-            value={userData.studentNumber}
-            onChange={handleInputChange}
-          />
-        ) : (
-          <span>{userData.studentNumber}</span>
-        )}
-      </div>
-      <div>
-        <label>Email:</label>
-        {editMode ? (
-          <input
-            type='text'
-            name='email'
-            value={userData.email}
-            onChange={handleInputChange}
-          />
-        ) : (
-          <span>{userData.email}</span>
-        )}
-      </div>
-      {editMode ? (
-        <>
-          <button onClick={handleUpdate}>Update</button>
-          <button onClick={handleCreateAlbum}>Create Album</button>
-        </>
-      ) : (
-        <button onClick={handleEdit}>Edit</button>
-      )}
-      {/* Render the albums and upload image functionality */}
-    </div>
+    </>
   );
 };
 
